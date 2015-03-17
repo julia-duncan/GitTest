1fa1edbc99752739a0d36a6f757f8e157c583352
Julia Duncan <jad667@mail.missouri.edu>
2015-03-17 12:43:08 -0500

    First Commit
    This is the first commit of our Xcode project

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Julia Duncan on 3/17/15.
+//  Copyright (c) 2015 Julia Duncan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
